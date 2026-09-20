20d44dc5f6b57bd8bd056776ac9864c8a99fe9d9
Aditya Bisht <adityabisht93686@gmail.com>
2026-09-20 23:17:48 +0530

    Time: 30 ms (44.18%), Space: 26 MB (43.05%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            else:
+                hashset.add(n)  
+        return False      
+
+                 
+
+        
